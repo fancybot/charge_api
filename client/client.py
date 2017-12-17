0093a10c13158cd4cf11606ff9cfcce6061e6007
liu <cumt_liucanbin@126.com>
2017-12-17 14:05:22 +0000

    add client

---
+++ client/client.py
@@ -0,0 +1,88 @@
+#coding:utf-8
+
+import socket
+import sys
+import time
+
+HOST = '165.227.67.149'
+# HOST =""
+PORT = 8000
+ADDR =(HOST,PORT)
+BUFSIZE = 1024
+
+MONEY="0.05"
+
+print "check api"
+sock = socket.socket()
+try:
+    a = sock.connect(ADDR)
+except Exception,e:
+    print 'error',e
+    sock.close()
+    sys.exit()
+
+print 'have connected with server'
+
+
+data="#REQ_ORDER_ID#"+MONEY
+if len(data)>0:
+    print 'send:',data
+    sock.sendall(data) #不要用send()
+    recv_data = sock.recv(BUFSIZE)
+    ID=recv_data
+    print recv_data
+    sock.close()
+sock.close()
+
+time.sleep(1)
+
+print "check api2"
+sock = socket.socket()
+try:
+    a = sock.connect(ADDR)
+except Exception,e:
+    print 'error',e
+    sock.close()
+    sys.exit()
+
+print 'have connected with server'
+
+data="#REQ_CODE#"+ID
+if len(data)>0:
+    print 'send:',data
+    sock.sendall(data) #不要用send()
+    recv_data = sock.recv(BUFSIZE)
+    img=recv_data
+    file = file("./temp.png", "wb")
+    try:
+        file.write(img)
+    finally:
+        file.close()
+    print "save img..."
+    sock.close()
+sock.close()
+
+
+for cnt in range(100):
+    time.sleep(3)
+    print "check api3==>"+str(cnt)
+    sock = socket.socket()
+    try:
+        a = sock.connect(ADDR)
+    except Exception,e:
+        print 'error',e
+        sock.close()
+        sys.exit()
+    print 'have connected with server'
+    data="#CHECK_STA#"+ID
+    if len(data)>0:
+        print 'send:',data
+        sock.sendall(data) #不要用send()
+        recv_data = sock.recv(BUFSIZE)
+        # print recv_data
+        if(recv_data=='Y'):
+            print "YES"
+        else:
+            print "NO"
+        sock.close()
+    sock.close()
